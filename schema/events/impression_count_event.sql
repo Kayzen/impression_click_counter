7ef2b008496acb6ea6da6d62abc569f2f505fee0
Devanandpatil <devanand.patil@kayzen.io>
2020-04-29 15:55:56 +0530

    changed expiry from 1 day to 30 days

--- schema/events/impression_count_event.sql
+++ schema/events/impression_count_event.sql
@@ -3,6 +3,7 @@ DELIMITER $$
 CREATE EVENT `expire_impressions_day_minus_15`
 ON SCHEDULE EVERY 2 MINUTE STARTS current_timestamp() DO
 BEGIN
-	CALL expire_fcap_events('impression', 15, 50000);
+	CALL expire_fcap_events('impression', 30, 50000);
+	CALL expire_fcap_events('click', 30, 50000);
 END$$
 DELIMITER ;
